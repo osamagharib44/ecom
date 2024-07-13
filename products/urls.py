c0b83f0c4b2e6aabbad8fbffeb65df2485611e8b
osama gharib <osamagharib45@gmail.com>
2024-07-13 20:51:04 +0300

    Updated urls

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products", views.ProductView.as_view() ,name="products-all"),
-    path("products/<int:pk>", views.ProductView.as_view() ,name="products-single"),
+    path("", views.ProductView.as_view() ,name="products-all"),
+    path("<int:pk>", views.ProductView.as_view() ,name="products-single"),
 
 ]
