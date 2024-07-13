9d7f4145aeda1c4250e0ada3ca6fa0e28740d660
osama gharib <osamagharib45@gmail.com>
2024-07-13 20:11:35 +0300

    Fixed views and urls

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products",views.ProductGetView,name="products-list")
+    path("products",views.ProductGetView.as_view() ,name="products-list"),
+    path("products/<int:pk>",views.ProductGetView.as_view() ,name="products-retrieve"),
+    path("products",views.ProductGetView.as_view() ,name="products-create"),
+    path("products/<int:pk>",views.ProductGetView.as_view() ,name="products-update"),
 ]
--- products/views.py
+++ products/views.py
@@ -3,13 +3,13 @@ from .models import Product
 from .serializers import ProductSerializer
 
 class ProductGetView(generics.GenericAPIView, mixins.ListModelMixin, mixins.RetrieveModelMixin):
-    model = Product
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = "pk"
     
     def get(self, request, *args, **kwargs):
         lookup_pk = kwargs.get(self.lookup_field)
         if (lookup_pk):
-            self.retrieve(request, *args, **kwargs)
-        self.list(request, *args, **kwargs)
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
         
