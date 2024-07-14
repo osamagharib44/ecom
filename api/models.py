437554eac730a11784492a62ea7ff3535b821997
osama gharib <osamagharib45@gmail.com>
2024-07-14 10:25:08 +0300

    Added stock field for products

--- api/models.py
+++ api/models.py
@@ -8,9 +8,11 @@ class Product(models.Model):
     title = models.CharField(max_length=50, db_index=True, unique=True)
     description = models.CharField(max_length=250, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=10)
     creatorId = models.ForeignKey(User, on_delete=models.CASCADE, related_name="products")
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
